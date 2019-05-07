0d5dee1aaa468c80785c4d55dadbcc6fc7bd562f
Will Murchison <wmurch@hotmail.com>
2019-05-07 14:55:04 -0400

    I have added everything

--- public/main.js
+++ public/main.js
@@ -12,31 +12,8 @@ const main = () => {
 
 document.addEventListener('DOMContentLoaded', main)
 
-const addTeam1Point = () => {
-  console.log('button was clicked')
-  Team1ScoreByClick += 1
-  document.querySelector('.team1Score').textContent = Team1ScoreByClick
+//update Team Names
 
-}
-
-const subTeam1Point = () => {
-  console.log('button was clicked')
-  Team1ScoreByClick -= 1
-  document.querySelector('.team1Score').textContent = Team1ScoreByClick
-
-}
-const addTeam2Point = () => {
-  console.log('button was clicked')
-  Team2ScoreByClick += 1
-  document.querySelector('.team2Score').textContent = Team2ScoreByClick
-}
-
-const subTeam2Point = () => {
-  console.log('button was clicked')
-  Team2ScoreByClick -= 1
-  document.querySelector('.team2Score').textContent = Team2ScoreByClick
-
-}
 const updateTeam1Name = () => {
   
   const team1Name = document.querySelector('#team1TxtInput').value
@@ -51,11 +28,87 @@ const updateTeam2Name = () => {
   document.querySelector('.team2Name').textContent = team2Name
 }
 
+//Add to or substract points from Team 1
+
+const addTeam1Point = () => {
+  console.log('button was clicked')
+  
+    if (Team1ScoreByClick < 21){
+      Team1ScoreByClick += 1
+      document.querySelector('.team1Score').textContent = Team1ScoreByClick
+      console.log('You can keep adding')
+    }
+    else{
+      document.querySelector('.result').textContent = 'Team 1 won the game'
+      document.querySelector('.team-1-add-1-button').disabled = true
+      document.querySelector('.team-1-subtract-1-button').disabled = true
+      document.querySelector('.team-2-add-1-button').disabled = true
+      document.querySelector('.team-2-subtract-1-button').disabled = true
+    }
+}
+const subTeam1Point = () => {
+  //ensure the number doesn't go below zero
+  console.log('button was clicked')
+  
+  if (Team1ScoreByClick <= 0) {
+    console.log('You cannot go below zero')
+  }
+  else {
+    Team1ScoreByClick -= 1
+    document.querySelector('.team1Score').textContent = Team1ScoreByClick
+  }
+}
+
+//Add to or substract points from Team 1
+
+const addTeam2Point = () => {
+  console.log('button was clicked')
+  
+    if (Team2ScoreByClick < 21){
+      Team2ScoreByClick += 1
+      document.querySelector('.team2Score').textContent = Team2ScoreByClick
+      console.log('You can keep adding')
+    }
+    else{
+      document.querySelector('.result').textContent = 'Team 2 won the game'
+      document.querySelector('.team-1-add-1-button').disabled = true
+      document.querySelector('.team-1-subtract-1-button').disabled = true
+      document.querySelector('.team-2-add-1-button').disabled = true
+      document.querySelector('.team-2-subtract-1-button').disabled = true
+    }
+}
+const subTeam2Point = () => {
+  console.log('button was clicked')
+  
+  if (Team2ScoreByClick <= 0) {
+    console.log('You cannot go below zero')
+  }
+  else {
+    Team2ScoreByClick -= 1
+    document.querySelector('.team2Score').textContent = Team2ScoreByClick
+  }
+}
+
+//reset all team scores and buttons
+
+const resetTeams = () => {
+  Team1ScoreByClick = 0
+  document.querySelector('.team1Score').textContent = Team2ScoreByClick
+  Team2ScoreByClick = 0
+  document.querySelector('.team2Score').textContent = Team2ScoreByClick
+
+  document.querySelector('.result').textContent = ''
+  document.querySelector('.team-1-add-1-button').disabled = false
+  document.querySelector('.team-1-subtract-1-button').disabled = false
+  document.querySelector('.team-2-add-1-button').disabled = false
+  document.querySelector('.team-2-subtract-1-button').disabled = false
+}
 
 document.querySelector('.team-1-add-1-button').addEventListener('click', addTeam1Point)
 document.querySelector('.team-1-subtract-1-button').addEventListener('click', subTeam1Point)
 document.querySelector('.team-2-add-1-button').addEventListener('click', addTeam2Point)
 document.querySelector('.team-2-subtract-1-button').addEventListener('click', subTeam2Point)
+document.querySelector('.reset-button').addEventListener('click', resetTeams)
 
 document
   .querySelector('.update-team-1-name')
